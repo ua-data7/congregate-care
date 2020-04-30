dcdd1a3fb842cf63b2cc9ca2419bb341c0ab3cc8
Chris Klimowski <csklimowski@email.arizona.edu>
2020-04-30 16:52:58 -0700

    index pages (auth and no auth)

--- care/frontend/views.py
+++ care/frontend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'app.html')
+    else:
+        return render(request, 'no_auth.html')
