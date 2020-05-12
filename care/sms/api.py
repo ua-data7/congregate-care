541d914596d593c62d1df1f0e90e33fd08511a1c
Nick Eddy <eddy.nicholas@gmail.com>
2020-05-12 12:28:52 -0600

    remove EnrolledUser model and refactor Facility - no need for individual users since Facility is the basis of reminders/notifications

--- care/sms/api.py
+++ care/sms/api.py
@@ -2,7 +2,6 @@ from django.conf import settings
 import requests
 from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
-from care.sms.models import EnrolledUser
 from care.sms.models import QualtricsSubmission
 from care.sms.models import TwilioConversation
 from care.sms.models import TwilioMessage
@@ -22,8 +21,8 @@ import pytz
 
 
 class QualtricsFormUpdateWebhookAPIView(generics.CreateAPIView):
-    queryset = EnrolledUser.objects.all()
-    serializer_class = serializers.EnrolledUserSerializer
+    queryset = Facility.objects.all()
+    serializer_class = serializers.FacilitySerializer
     permission_classes = (permissions.AllowAny,)
 
     def create(self, request, *args, **kwargs):
@@ -34,34 +33,26 @@ class QualtricsFormUpdateWebhookAPIView(generics.CreateAPIView):
         # uuid is embedded data in the qualtrics survey
         uuid = request.data.get('uuid', None)
         if uuid:
-            user = EnrolledUser.objects.get(identity=uuid)
+            facility = Facility.objects.get(identity=uuid)
             reporting_new_cases = request.data.get('reporting_new_cases', None)
             reporting_new_cases = reporting_new_cases == 'Yes'
             filename = request.data.get('filename', None)
-            user.reporting_new_cases = reporting_new_cases
+            facility.reporting_new_cases = reporting_new_cases
             new_cases = request.data.get('new_cases', None)
             if new_cases is None:
                 new_cases = 0
-            user.last_new_cases_reported = new_cases
+            facility.last_new_cases_reported = new_cases
             if filename and len(filename) > 0:
-                user.last_upload_date = az_now
-            user.save()
-        else:
-            user = None
-        facility_name = request.data.get('facility_name', None)
-        if facility_name:
-            facility_name = facility_name.lower()
-        facility = Facility.objects.filter(name__lower__contains=facility_name)
-        if facility.exists():
-            facility = facility[0]
+                facility.last_upload_date = az_now
+            facility.save()
         else:
             facility = None
+        facility_name = request.data.get('facility_name', None)
         qualtrics_submission = QualtricsSubmission.objects.create(
             facility=facility,
             facility_name=facility_name,
             new_cases=new_cases,
             reporting_new_cases=reporting_new_cases,
-            reported_by=user
         )
         return Response(status=status.HTTP_200_OK)
 
@@ -96,15 +87,15 @@ class TwilioConversationCallbackAPIView(generics.CreateAPIView):
             conversation = TwilioConversation.objects.get(sid=conversation_sid)
             try:
                 binding = Binding.objects.get(address=author)
-                user = binding.user
+                facility = binding.facility
             except Binding.DoesNotExist:
                 # address is our own Twilio number we're replying from.
-                user = None
+                facility = None
             TwilioMessage.objects.create(
                 index=int(index),
                 conversation=conversation,
                 body=body,
-                user=user,
+                facility=facility,
                 author_sid=participant_sid,
             )
         return Response(status=status.HTTP_200_OK)
@@ -117,8 +108,8 @@ class TwilioConversationListAPIView(generics.ListAPIView):
 
 
 class SendSMSMessageAPIView(generics.CreateAPIView):
-    queryset = EnrolledUser.objects.none()
-    serializer_class = serializers.EnrolledUserSerializer
+    queryset = Facility.objects.none()
+    serializer_class = serializers.FacilitySerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
@@ -132,8 +123,8 @@ class SendSMSMessageAPIView(generics.CreateAPIView):
 
 
 class SendEmailMessageAPIView(generics.CreateAPIView):
-    queryset = EnrolledUser.objects.none()
-    serializer_class = serializers.EnrolledUserSerializer
+    queryset = Facility.objects.none()
+    serializer_class = serializers.FacilitySerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
--- care/sms/management/commands/send_reminder_message.py
+++ care/sms/management/commands/send_reminder_message.py
@@ -1,4 +1,5 @@
 from care.sms.models import send_sms_message, send_email_message
+from care.sms.models import Facility
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
@@ -7,9 +8,9 @@ class Command(BaseCommand):
     help = 'Sends a reminder with link to survey to SMS/email users'
 
     def handle(self, *args, **options):
-        for user in EnrolledUser.objects.filter(preferred_contact=PreferredContactType.SMS):
-            message = f'Please update your facility status at {settings.QUALTRICS_SURVEY_LINK}?uuid={user.uuid}'
-            send_sms_message(user.identity, message, bulk=False)
-        for user in EnrolledUser.objects.filter(preferred_contact=PreferredContactType.EMAIL):
-            message = f'Hello {user.name},\nPlease update your facility status at {settings.QUALTRICS_SURVEY_LINK}?uuid={user.uuid}\nRegards, Pima County Health Department'
-            send_email_message(user.uuid, settings.DEFAULT_REMINDER_EMAIL_SUBJECT, message, bulk=False)
+        for facility in Facility.objects.filter(preferred_contact=PreferredContactType.SMS):
+            message = f'Please update your facility status at {settings.QUALTRICS_SURVEY_LINK}?uuid={facility.identity}'
+            send_sms_message(facility.identity, message, bulk=False)
+        for facility in Facility.objects.filter(preferred_contact=PreferredContactType.EMAIL):
+            message = f'Hello {facility.name},\nPlease update your facility status at {settings.QUALTRICS_SURVEY_LINK}?uuid={facility.identity}\nRegards, Pima County Health Department'
+            send_email_message(facility.identity, settings.DEFAULT_REMINDER_EMAIL_SUBJECT, message, bulk=False)
