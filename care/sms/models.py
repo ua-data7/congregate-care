e34ac24e32a072084484c677f33535c36a6023a9
Nick Eddy <eddy.nicholas@gmail.com>
2020-05-20 12:48:53 -0600

    reply-to must be a list or tuple

--- care/sms/models.py
+++ care/sms/models.py
@@ -162,14 +162,14 @@ def send_email_message(uuid, subject, message, attachment_filename=None, attachm
                 for email in facility.split(','):
                     emails.append(email)
             # subject, message, from, to, to_cc, to_bcc,
-            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], None, emails[1:], reply_to=settings.SENDGRID_REPLY_TO_EMAIL)
+            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], None, emails[1:], reply_to=[settings.SENDGRID_REPLY_TO_EMAIL])
     else:
         facility = Facility.objects.get(identity=uuid)
         emails = facility.emails.split(',')
         if len(emails) > 1:
-            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], None, emails[1:], reply_to=settings.SENDGRID_REPLY_TO_EMAIL)
+            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], None, emails[1:], reply_to=[settings.SENDGRID_REPLY_TO_EMAIL])
         else:
-            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], reply_to=settings.SENDGRID_REPLY_TO_EMAIL)
+            email_message = EmailMessage(subject, message, settings.SENDGRID_FROM_EMAIL, [emails[0]], reply_to=[settings.SENDGRID_REPLY_TO_EMAIL])
     if email_message:
         if attachment_filename is not None and attachment_content is not None and attachment_mimetype is not None:
             email_message.attach(attachment_filename, attachment_content, attachment_mimetype)
